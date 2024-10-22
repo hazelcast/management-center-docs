0825b5a7b8b80ce8f5b692a494d16337a8102619
Mykola Fant <mykola.fant@hazelcast.com>
2024-10-22 11:55:47 +0300

    add check-links script

--- fetch-check-links-playbook.js
+++ fetch-check-links-playbook.js
@@ -4,4 +4,11 @@ if (githubRepo.endsWith('.js') || githubBaseBranch.endsWith('.js')) {
 	throw new Error('GitHub repository name and base branch should be passed as arguments');
 }
 
-console.log(githubRepo, githubBaseBranch);
+console.log('Repository name: ', githubRepo);
+console.log('Base branch: ', githubBaseBranch);
+
+// 1. Load and parse local antora-playbook.yml
+// 2. Load and parse global antora-playbook.yml's content.sources
+// 3. Modify global content.sources
+// 		- 
+// 4. Replace local content.sources with the modified content.sources
